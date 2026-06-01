1740500dea4660a4ef339902dc861b5b740e3ed8
Prathmesh Naiknaware <89856936+prathmeshcool@users.noreply.github.com>
2026-06-01 11:39:34 +0530

    Add: jump-game - submission-1

---
+++ Data Structures & Algorithms/jump-game/submission-1.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+        int n = nums.size();
+
+        int mx = 0;
+
+        for(int i=0; i<n; i++){
+            if(mx < i)
+                return false;
+            mx = max(mx, i + nums[i]);
+        }
+
+        return (mx>=n-1);
+    }
+};
